056ada9b76b858c4c301aa59bec496bad63a749b
Violent <128183707+realviolent@users.noreply.github.com>
2025-07-23 07:11:04 +0200

    Added AWS

--- Stratum_Loader.lua
+++ Stratum_Loader.lua
@@ -2,7 +2,9 @@ local gameIDS = {
     [109983668079237] = "Steal_a_Brainrot",
     [74866348003583] = "Steal_a_Pet",
     [12177325772] = "Super_League_Soccer",
-    [101914543102200] = "Tap_Infinity"
+    [101914543102200] = "Tap_Infinity",
+    [13127800756] = "Arm_Wrestle_Simulator"
+    
 }
 
 local currentgame = gameIDS[game.PlaceId]
@@ -16,5 +18,7 @@ if currentgame then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Sub2BK/Stratum/refs/heads/Scripts/Super_League_Soccer.lua"))()
     elseif currentgame == "Tap_Infinity" then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Sub2BK/Stratum/refs/heads/Scripts/Tap_Infinity.lua"))()
+    elseif currentgame == "Arm_Wrestle_Simulator" then
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/Sub2BK/Stratum/refs/heads/Scripts/Arm_Wrestle_Simulator.lua"))()
     end
 end
