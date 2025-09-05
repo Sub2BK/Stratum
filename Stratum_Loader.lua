77579cdea50e074628d12fe05210619dc2224acb
Sub2BK <discordbkmdytt@gmail.com>
2025-09-05 16:58:54 +0400

    Update Stratum_Loader.lua

--- Stratum_Loader.lua
+++ Stratum_Loader.lua
@@ -1,15 +1,9 @@
-local gameIDS = {
-    [109983668079237] = "Steal_a_Brainrot",
-    [74866348003583] = "Steal_a_Pet",
-    [126195208568849] = "Super_League_Soccer",
-    [12177325772] = "Super_League_Soccer",
-    [101914543102200] = "Tap_Infinity"
-}
-
-local game = cloneref(game)
 local getgenv = getgenv
+local game = cloneref(game)
 
-wait(game:IsLoaded())
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
 
 if not getgenv().Config then
     getgenv().Config = {
@@ -17,36 +11,33 @@ if not getgenv().Config then
     }
 end
 
+local GameIDS = {
+    [74866348003583] = "Steal_a_Pet",
+    [126195208568849] = "Super_League_Soccer",
+    [12177325772] = "Super_League_Soccer",
+    [101914543102200] = "Tap_Infinity"
+}
+
 local get_service = function(service_name)
-    local services = getgenv().Services or {}
-    local service = services[service_name]
-    if not service then
-        service = cloneref(game:GetService(service_name))
-        services[service_name] = service
+    getgenv().Services = getgenv().Services or {}
+    if not getgenv().Services[service_name] then
+        getgenv().Services[service_name] = cloneref(game:GetService(service_name))
     end
-    getgenv().Services = services
-    return service
+    return getgenv().Services[service_name]
 end
 
-local httpService = get_service("HttpService")
+local gameName = GameIDS[game.PlaceId]
 
-local currentgame = gameIDS[game.PlaceId]
+if gameName and getgenv().Config.Enabled then
+    local url = "https://raw.githubusercontent.com/Sub2BK/Stratum/refs/heads/Scripts/" .. gameName .. ".lua"
 
-if currentgame and getgenv().Config.Enabled then
-    local url = "https://raw.githubusercontent.com/Sub2BK/Stratum/refs/heads/Scripts/" .. currentgame .. ".lua"
-    local success, script_content = pcall(function()
-        return game.HttpGet(url)
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(url))()
     end)
-    if success and script_content then
-        local loadsuccess, loadedscript = pcall(loadstring(script_content))
-        if loadsuccess then
-            loadedscript()
-        else
-            print("Stratum loader: Failed to load script for " .. currentgame .. ". Error: " .. loadedscript)
-        end
-    else
-        print("Stratum loader: Failed to download script from " .. url)
+
+    if not success then
+        warn("Loader failed for " .. gameName .. ". Reason: " .. tostring(err))
     end
 else
-    print("Stratum loader: The current game (ID: " .. game.PlaceId .. ") is not in the supported list or script is disabled.")
+    print("Loader: Game not supported or script is disabled.")
 end
