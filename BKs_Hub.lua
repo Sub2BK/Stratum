09a04de3dde65e0b441cfc640b204044c4f239f0
Sub2BK <discordbkmdytt@gmail.com>
2025-11-30 14:33:42 +0400

    Update BKs_Hub.lua

--- BKs_Hub.lua
+++ BKs_Hub.lua
@@ -1,1 +1,1 @@
-(loadstring or load)(game:HttpGet("https://scriptprotector.vercel.app/api/raw/7e7d3876d68143b4a3bfc00a037096e1"))()
+(loadstring or load)(game:HttpGet("https://scriptprotector.vercel.app/api/raw/38ae55bb05db587605b459076bd54215"))()
